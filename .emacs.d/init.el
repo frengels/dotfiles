8dea3e4341873c25719679132e388e10b3cd9eec
Frederik <frederik.engels24@gmail.com>
2018-07-15 13:32:51 +0200

    started my basic emacs config

---
+++ .emacs.d/init.el
@@ -0,0 +1,20 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
+ '(line-number-mode nil)
+ '(package-selected-packages
+   (quote
+    (linum-off linum-relative spacemacs-theme slime powerline org-ref material-theme markdown-mode magit jedi idle-require helm-projectile helm-dash helm-company evil-surround evil-escape auto-compile))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(load-file (concat user-emacs-directory "settings.elc"))
