7dfe262ac07dd9285ae59ceb46d96972a96b3c7b
Frederik Engels <frederik.engels92@gmail.com>
2019-01-05 04:38:54 +0100

    add git info

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env sh
 
 stowables=(
+  git
   sway
 )
 
-for stowable in $stowables
+for stowable in ${stowables[@]}
 do
-	stow $stowable
+	stowoutput=$(stow $stowable)
+	stowres=$?
+	echo -n "stowing \"$stowable\""
+	if [ $stowres -eq 0 ]
+	then
+		echo -e " - \e[1;32mDONE\e[0m"
+	else
+		echo -e " - \e[1:31mFAIL\e[0m"
+	fi
 done
