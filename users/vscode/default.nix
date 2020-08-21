59872b5f2569a688263225e640b5a098e3f0aa20
Frederik Engels <frederik.engels92@gmail.com>
2020-08-21 15:00:12 +0200

    make changes to vscode config

--- users/vscode/default.nix
+++ users/vscode/default.nix
@@ -1,12 +1,38 @@
 { config, lib, pkgs, ... }:
 let
   cfg = config.programs.vscode;
+
+  vscode-clangd = pkgs.vscode-utils.buildVscodeMarketplaceExtension rec {
+
+    mktplcRef = {
+      name = "vscode-clangd";
+      publisher = "llvm-vs-code-extensions";
+      version = "0.1.6";
+      sha256 = "07bf54f19b5c9385014dfeee47015c73b6d3bac7f95db6d005f91bd5e7123c07";
+    };
+
+    buildInputs = [
+      pkgs.clang-tools
+    ];
+
+    postPatch = ''
+      # patch packages.json to use nix's clang
+      substituteInPlace "./package.json" \
+        --replace "\"default\": \"clangd\"" "\"default\": \"${pkgs.clang-tools}/bin/clangd\""
+    '';
+
+    meta = with pkgs.stdenv.lib; {
+      description = "Provides C/C++ language IDE features for VS Code using clangd";
+      license = licenses.mit;
+    };
+  };
 in {
   config = lib.mkIf cfg.enable {
     nixpkgs.config.allowUnfree = true;
 
     programs.vscode = {
-      package = pkgs.vscodium;
+      # disable for now as it doesn't show all extensions
+      # package = pkgs.vscodium;
       extensions = with pkgs.vscode-extensions; [
         bbenoist.Nix 
         ms-vscode.cpptools
@@ -16,7 +42,12 @@ in {
         matklad.rust-analyzer
         llvm-org.lldb-vscode
         james-yu.latex-workshop
+	vscode-clangd
       ];
     };
+
+    home.packages = with pkgs; [
+      clang-tools
+    ];
   };
 }
