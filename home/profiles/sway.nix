344e3d8989b4187a646d96f1a40fb2eca7a09ab6
Frederik Engels <frederik.engels92@gmail.com>
2020-08-25 14:44:19 +0200

    use workspace 10 instead of 0 for 0 key

--- home/profiles/sway.nix
+++ home/profiles/sway.nix
@@ -74,7 +74,6 @@ in {
           "${mod}+Shift+space" = "floating toggle";
           "${mod}+space" = "focus mode_toggle";
 
-          "${mod}+0" = "workspace number 0";
           "${mod}+1" = "workspace number 1";
           "${mod}+2" = "workspace number 2";
           "${mod}+3" = "workspace number 3";
@@ -84,6 +83,7 @@ in {
           "${mod}+7" = "workspace number 7";
           "${mod}+8" = "workspace number 8";
           "${mod}+9" = "workspace number 9";
+          "${mod}+0" = "workspace number 10";
 
           "${mod}+Shift+1" = "move container to workspace number 1";
           "${mod}+Shift+2" = "move container to workspace number 2";
@@ -94,6 +94,7 @@ in {
           "${mod}+Shift+7" = "move container to workspace number 7";
           "${mod}+Shift+8" = "move container to workspace number 8";
           "${mod}+Shift+9" = "move container to workspace number 9";
+	  "${mod}+Shift+0" = "move container to workspace number 10";
 
           "${mod}+Shift+minus" = "move scratchpad";
           "${mod}+minus" = "scratchpad show";
