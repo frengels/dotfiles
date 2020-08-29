d1c80f2f48848714d87cfe844054657331718e5b
Frederik Engels <frederik.engels92@gmail.com>
2020-08-29 10:58:17 +0200

    remove commented out sections

--- profiles/networking/wireless/default.nix
+++ profiles/networking/wireless/default.nix
@@ -3,22 +3,8 @@
   networking.networkmanager.enable = true;
   networking.networkmanager.wifi.backend = "iwd";
 
-  /*
-  environment.etc."NetworkManager/system-connections/karola-devolo.nmconnection" = {
-    source = ../../../secrets/wireless/karola-devolo;
-    mode = "0400";
-  };
-  */
-
   environment.etc."NetworkManager/system-connections/karola-devolo.nmconnection" =
     import ../../../secrets/wireless/karola-devolo.nix;
-
-  /*
-  environment.etc."NetworkManager/system-connections/karola-fritzbox.nmconnection" = {
-    source = ../../../secrets/wireless/karola-fritzbox;
-    mode = "0400";
-  };
-  */
 
   environment.etc."NetworkManager/system-connections/karola-fritzbox.nmconnection" =
     import ../../../secrets/wireless/karola-fritzbox.nix;
