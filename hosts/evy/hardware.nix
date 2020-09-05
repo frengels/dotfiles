4625eb42ea09aca26386f0f18257186504ec4e65
Frederik Engels <frederik.engels92@gmail.com>
2020-09-05 16:00:42 +0200

    set noatime to root partition

--- hosts/evy/hardware.nix
+++ hosts/evy/hardware.nix
@@ -16,6 +16,7 @@
   fileSystems."/" =
     { device = "/dev/disk/by-uuid/c7745ef1-af0d-4750-9359-aa5ff5477970";
       fsType = "btrfs";
+      options = [ "noatime" ];
     };
 
   fileSystems."/boot" =
