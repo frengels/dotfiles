e237cd1db3963d3d2c15e22d2cbbe449b18ba3ad
Frederik Engels <frederik.engels92@gmail.com>
2020-10-14 04:28:39 +0200

    enable vaapi for chromium

--- home/profiles/chromium.nix
+++ home/profiles/chromium.nix
@@ -9,6 +9,7 @@ in {
   config = with lib; mkIf cfg.enable {
     programs.chromium = {
       enable = true;
+      package = (pkgs.chromium.override { enableVaapi = true; });
       extensions = [
         "cjpalhdlnbpafiamejdnhcphjbkeiagm" # ublock origin
         "nngceckbapebfimnlniiiahkandclblb" # bitwarden
